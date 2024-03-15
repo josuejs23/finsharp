ee7b0c62cbfcdabed8b42b33d1679d6ad03dc954
Josue Zorrilla <josuezorrilla2302@gmail.com>
2024-03-15 15:04:38 -0400

    Added style to components

--- fronted/src/components/Card/Card.tsx
+++ fronted/src/components/Card/Card.tsx
@@ -15,18 +15,16 @@ const Card: React.FC<Props> = ({
   onPortfolioCreate,
 }: Props): JSX.Element => {
   return (
-    <div className="card">
-      <img
-        src="https://images.unsplash.com/photo-1620321023374-d1a68fbc720d?q=80&w=2994&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="logo"
-      />
-      <div className="details">
-        <h2>
-          {searchResult.name} ({searchResult.symbol})
-        </h2>
-        <p>{searchResult.symbol}</p>
-      </div>
-      <p className="info">
+    <div
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <h2 className="font-bold text-center text-black md:text-left">
+        {searchResult.name} ({searchResult.symbol})
+      </h2>
+      <p className="text-black">{searchResult.currency}</p>
+      <p className="font-bold text-black">
         {searchResult.exchangeShortName} - {searchResult.stockExchange}
       </p>
       <AddPortfolio
--- fronted/src/components/Portfolio/ListPortfolio/ListPortfolio.tsx
+++ fronted/src/components/Portfolio/ListPortfolio/ListPortfolio.tsx
@@ -8,19 +8,29 @@ interface Props {
 
 const ListPortfolio = ({ portfolioValues, onDeletePortfolioValue }: Props) => {
   return (
-    <div>
-      {portfolioValues.length > 0 ? (
-        portfolioValues.map((value) => (
-          <CardPortfolio
-            key={value}
-            onDeletePortfolioValue={onDeletePortfolioValue}
-            portfolioValue={value}
-          />
-        ))
-      ) : (
-        <h2>No data</h2>
-      )}
-    </div>
+    <section id="portfolio">
+      <h2 className="mb-3 mt-3 text-3xl font-semibold text-center md:text-4xl">
+        My Portfolio
+      </h2>
+      <div className="relative flex flex-col items-center max-w-5xl mx-auto space-y-10 px-10 mb-5 md:px-6 md:space-y-0 md:space-x-7 md:flex-row">
+        <>
+          {portfolioValues.length > 0 ? (
+            portfolioValues.map((portfolioValue) => {
+              return (
+                <CardPortfolio
+                  portfolioValue={portfolioValue}
+                  onDeletePortfolioValue={onDeletePortfolioValue}
+                />
+              );
+            })
+          ) : (
+            <h3 className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+              Your portfolio is empty.
+            </h3>
+          )}
+        </>
+      </div>
+    </section>
   );
 };
 
