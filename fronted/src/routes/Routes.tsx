3d6c20cde171a5f7c204e6511784d2413a90e48e
Josue Zorrilla <josuezorrilla2302@gmail.com>
2024-03-16 16:04:48 -0400

    Added subroutes to dashboard

--- fronted/src/routes/Routes.tsx
+++ fronted/src/routes/Routes.tsx
@@ -3,6 +3,8 @@ import App from "../App";
 import HomePage from "../pages/HomePage/HomePage";
 import SearchPage from "../pages/SearchPage/SearchPage";
 import CompanyPage from "../pages/CompanyPage/CompanyPage";
+import IncomeStatement from "../components/IncomeStatement/IncomeStatement";
+import CompanyProfile from "../components/CompanyProfile/CompanyProfile";
 
 export const router = createBrowserRouter([
   {
@@ -11,7 +13,14 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <HomePage /> },
       { path: "search", element: <SearchPage /> },
-      { path: "company/:ticker", element: <CompanyPage /> },
+      {
+        path: "company/:ticker",
+        element: <CompanyPage />,
+        children: [
+          { path: "company-profile", element: <CompanyProfile /> },
+          { path: "income-statement", element: <IncomeStatement /> },
+        ],
+      },
     ],
   },
 ]);
