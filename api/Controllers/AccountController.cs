722ba88864a2391ad2159a7ffc91a36736b41e41
Josue Zorrilla <josuezorrilla2302@gmail.com>
2024-03-21 23:24:38 -0400

    Added authentication and authorization

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using api.DTOs.Account;
+using api.Interfaces;
 using api.models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -15,10 +16,12 @@ namespace api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser>  _userManager;
+        private readonly ITokenService  _tokenService;
 
-        public AccountController(UserManager<AppUser> userManager)
+        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
         }   
 
         [HttpPost("register")]
@@ -42,7 +45,14 @@ namespace api.Controllers
                     var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
                     if(roleResult.Succeeded)
                     {
-                        return Ok("User created.");
+                        return Ok(
+                            new NewUserDto
+                            {
+                                UserName = appUser.UserName,
+                                Email = appUser.Email,
+                                Token = _tokenService.CreateToken(appUser)
+                            }
+                        );
                     } 
                     else
                     {
