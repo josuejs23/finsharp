77d326620bdb2e3a0b73a737ef959b804f5f6232
Josue Zorrilla <josuezorrilla2302@gmail.com>
2024-03-21 21:33:06 -0400

    Added feature to register user

---
+++ api/Migrations/20240321231623_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "8471b9ef-d3e2-4c25-b6cb-af920679c41c", null, "User", "USER" },
+                    { "ed75f17a-059f-4189-8dba-a7aef12212e1", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8471b9ef-d3e2-4c25-b6cb-af920679c41c");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ed75f17a-059f-4189-8dba-a7aef12212e1");
+        }
+    }
+}
