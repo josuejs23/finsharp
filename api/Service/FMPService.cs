c51dda69311267fe9ea07395f7e49614be02ca5d
Josue Zorrilla <josuezorrilla2302@gmail.com>
2024-03-30 21:06:15 -0400

    Fixed the FMP service error

--- api/Service/FMPService.cs
+++ api/Service/FMPService.cs
@@ -26,7 +26,7 @@ namespace api.Service
         {
             try
             {
-                var result = await _httpClient.GetAsync($"https://financialmodelingprep.com/api/v3/cash-flow-statement/${symbol}?limit=40&apikey={_config["FMPKey"]}");
+                var result = await _httpClient.GetAsync($"https://financialmodelingprep.com/api/v3/profile/{symbol}?apikey={_config["FMPKey"]}");
                 if(result.IsSuccessStatusCode)
                 {
                     var content = await result.Content.ReadAsStringAsync();
@@ -34,7 +34,7 @@ namespace api.Service
                     var stock = tasks[0];
                     if(stock != null)
                     {
-                        stock.ToStockFromFMP();
+                        return stock.ToStockFromFMP();
                     }
                     return null;
                 }
